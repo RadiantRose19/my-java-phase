681dc677a0e3da9d68c160fe8ba9d29110274841
RadiantRose19 <bavya19.bs05@gmail.com>
2025-11-30 18:11:35 +0530

    val_presence_check.java
    checks if a val is present inside the array of not

---
+++ val_presence_check.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,12,7,1};
+        int val= 12;
+        
+        for(int i=0;i<arr.length;i++) {
+            if(val == arr[i]) System.out.print(val+ " is present");
+        }
+    }
+}
