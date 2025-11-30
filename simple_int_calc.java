acb3ae6e97ae53efc3e7e977dd4c2886181ae32f
RadiantRose19 <bavya19.bs05@gmail.com>
2025-11-30 17:42:49 +0530

    simple_int_calc.java

---
+++ simple_int_calc.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int p = 10;
+        int r = 20;
+        int t = 5;
+        
+        int SI = p*r*t;
+        System.out.println("SI: "+SI);
+    }
+}
