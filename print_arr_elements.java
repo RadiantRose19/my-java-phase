8f942c5baa57de3c0dfa64af0b678c5e97fe57ce
RadiantRose19 <bavya19.bs05@gmail.com>
2025-11-30 17:56:01 +0530

    print_arr_elements.java
    prints all the arr elements from the given array

---
+++ print_arr_elements.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,12,3,1};
+        
+        for(int i=0;i<arr.length;i++) {
+        System.out.println(arr[i]);
+        }
+    }
+}
