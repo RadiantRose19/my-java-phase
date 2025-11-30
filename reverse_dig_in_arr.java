046e4c328038d577193540451922e9cdb2b2f386
RadiantRose19 <bavya19.bs05@gmail.com>
2025-11-30 18:08:56 +0530

    reverse_dig_in_arr.java
    given an arr, prints all the digits in reverse in an array

---
+++ reverse_dig_in_arr.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,12,7,1};
+        int rev = 0;
+        
+        for(int i=arr.length-1;i>=0;i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
