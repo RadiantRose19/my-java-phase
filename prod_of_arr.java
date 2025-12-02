29d7a6b55385ab79a297c8f90f9c3dd232c9f306
RadiantRose19 <bavya19.bs05@gmail.com>
2025-12-02 21:10:30 +0530

    prod_of_arr.java

---
+++ prod_of_arr.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,5};
+        int prod = 1;
+        
+        for(int i=0;i<arr.length;i++) {
+            prod = prod * arr[i];
+            System.out.println(prod);
+        }
+    }
+}
